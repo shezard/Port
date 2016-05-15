a182741f74644af1491cda54c9f78f0e5b74dd0a
damien fayol <dam.fayol@gmail.com>
2016-05-15 09:58:06 +0200

    Simplify world render internals

--- src/game/world.js
+++ src/game/world.js
@@ -5,11 +5,20 @@ var World = function(el, mapSize) {
   this.width = el.width;
   this.height = el.height;
   this.mapSize = mapSize;
+  this.sightRange = 4;
+
+  this.shapes = [
+    [{x:0, y:40, w:60, h:this.height-80},{},                                                       {x:this.width-60, y:50, w:60, h:this.height-80}],
+    [{x:0, y:30, w:50, h:this.height-60}, {x:(this.width-100) / 2, y:30,w:100, h:this.height - 60},{x:this.width-50, y:30, w:50, h:this.height-60}],
+    [{x:0, y:20, w:40, h:this.height-40}, {x:(this.width-120) / 2, y:20,w:120, h:this.height - 40},{x:this.width-40, y:20, w:40, h:this.height-40}],
+    [{x:0, y:10, w:30, h:this.height-20}, {x:(this.width-140) / 2, y:10,w:140,h:this.height - 20}, {x:this.width-30, y:10, w:30, h:this.height-20}],
+    [{x:0, y:0,  w:20, h:this.height},    {x:(this.width-140) / 2, y:0, w:140,h:this.height},      {x:this.width-20, y:0,  w:20, h:this.height}],
+  ]
 }
 
 World.prototype.render = function(map, player, entities) {
 
-  var cols = [];
+  this.drawBackground();
 
   var depth = [-1, 1, 0];
 
@@ -17,59 +26,36 @@ World.prototype.render = function(map, player, entities) {
 
     var j = depth[d];
 
-    for(var i = 5 ; i >= 0 ; i -= 1) {
+    for(var i = this.sightRange ; i >= 0 ; i -= 1) {
 
       var hit = {
-        x: player.x + .5 + player.d.x * i + (1 - Math.abs(player.d.x)) * j,
-        y: player.y + .5 + player.d.y * i + (1 - Math.abs(player.d.y)) * j
+        x: player.x + 1 + player.d.x * i + (1 - Math.abs(player.d.x)) * j,
+        y: player.y + 1 + player.d.y * i + (1 - Math.abs(player.d.y)) * j
       }
+
+      console.log(hit);
 
       var collide = false;
 
       for(var x = 0 ; x < this.mapSize ; x++) {
         for(var y = 0 ; y < this.mapSize ; y++) {
-          if(map[y][x] && x < hit.x && hit.x <= (x + 1) && y <= hit.y && hit.y <= (y + 1)) {
-            collide = map[y][x];
+          if(map[y][x] && x < hit.x && hit.x <= (x + 1) && y < hit.y && hit.y <= (y + 1)) {
+            var color = map[y][x];
+
+            var wallX = this.sightRange - i;
+            var wallY = j + 1;
+            if(player.d.x == -1 && player.d.y == 0 || player.d.x == 0 && player.d.y == 1) {
+              wallY = -j + 1;
+            }
+
+            var shape = this.shapes[wallX][wallY];
+
+            this.draw(shape, color);
           }
         }
       }
-
-      if(collide) {
-
-        var x = 4 - i;
-        var y = j + 1;
-        if(player.d.x == -1 && player.d.y == 0 || player.d.x == 0 && player.d.y == 1) {
-          y = -j + 1;
-        }
-
-        cols.push({
-          x: x,
-          y: y,
-          color: collide
-        });
-      }
-
     }
   }
-
-  var shapes = [
-    [{x:0, y:40, w:60, h:this.height-80},{},                                                       {x:this.width-60, y:50, w:60, h:this.height-80}],
-    [{x:0, y:30, w:50, h:this.height-60}, {x:(this.width-100) / 2, y:30,w:100, h:this.height - 60},{x:this.width-50, y:30, w:50, h:this.height-60}],
-    [{x:0, y:20, w:40, h:this.height-40}, {x:(this.width-120) / 2, y:20,w:120, h:this.height - 40},{x:this.width-40, y:20, w:40, h:this.height-40}],
-    [{x:0, y:10, w:30, h:this.height-20}, {x:(this.width-140) / 2, y:10,w:140,h:this.height - 20}, {x:this.width-30, y:10, w:30, h:this.height-20}],
-    [{x:0, y:0,  w:20, h:this.height},    {x:(this.width-140) / 2, y:0, w:140,h:this.height},      {x:this.width-20, y:0,  w:20, h:this.height}],
-  ]
-
-  this.drawBackground();
-
-  for(var i = 0 ; i < cols.length ; i++) {
-    var col = cols[i];
-
-    var shape = shapes[col.x][col.y];
-
-    this.draw(shape, col.color);
-  }
-
 }
 
 World.prototype.drawBackground = function() {
--- test/src/game/world.spec.js
+++ test/src/game/world.spec.js
@@ -4,11 +4,30 @@ describe('World', function() {
   it('should be initialized correctly', function() {
 
     var el = document.createElement('canvas');
-
     var world = new World(el, 5);
 
     expect(world.width).toBe(300);
     expect(world.height).toBe(150);
     expect(world.mapSize).toBe(5);
+  });
+
+  it('should render correctly', function() {
+
+    var el = document.createElement('canvas');
+    var world = new World(el, 3);
+
+    world.render([
+      [1,1,1],
+      [1,0,1],
+      [1,1,1]
+    ], {
+      x: 1,
+      y: 1,
+      d: {
+        x: 1,
+        y: 0
+      }
+    }, [])
+
   });
 })
